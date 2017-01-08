58b86aa6260bf11973d27e40663b319172d66631
Satanimal <bartlomiej.podsiadly@o2.pl>
2017-01-08 15:50:51 +0100

    Graphics and connection stuff

--- src/Client/BeforeClient.java
+++ src/Client/BeforeClient.java
@@ -1,12 +1,25 @@
 package Client;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+import javax.swing.JOptionPane;
 
 public class BeforeClient{
 	//klasa ladujaca dane z serwera(liste gier) 
 	// i wyswietlajaca to w ramce 
 	//mozliwosc dolaczenia do gry i start glownego okienka jesli poprawne
 	public static void main(String[] args) {
-		
+		String serverAddress = JOptionPane.showInputDialog(
+	            "Enter IP Address of a machine that is\n" +
+	            "running the date service on port 9090:");
+	        Socket s = new Socket(serverAddress, 9090);
+	        BufferedReader input =
+	            new BufferedReader(new InputStreamReader(s.getInputStream()));
+	        String answer = input.readLine();
+	        JOptionPane.showMessageDialog(null, answer);
+	        System.exit(0);
 
 	}
 
---
+++ src/Graphics/BeforeClientFrame.java
@@ -0,0 +1,14 @@
+package Graphics;
+
+import javax.swing.JFrame;
+
+import Client.BeforeClient;
+
+public class BeforeClientFrame extends JFrame{
+	private BeforeClient beforeClient;
+	
+	public BeforeClientFrame(BeforeClient beforeClient){
+		this.beforeClient = beforeClient;
+	}
+
+}
